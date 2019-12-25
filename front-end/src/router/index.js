fa0d7078846b2c0031646450d9481efa72f1bc36
Faizan <qureshi@peerbits.com>
2019-12-25 12:54:56 +0530

    feat: routes and header and helper function done

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,24 +1,44 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Edit from '../views/Edit.vue'
+import New from '../views/New.vue'
+import Show from '../views/Show.vue'
+import Test from '../views/Test.vue'
+import Words from '../views/Words.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+      {
+        path: '/',
+        redirect: '/words'
+      },
+      {
+        path: '/words',
+        name: 'words',
+        component: Words
+      },
+      {
+        path: '/words/new',
+        name: 'new-word',
+        component: New
+      },
+      {
+        path: '/words/:id',
+        name: 'show',
+        component: Show
+      },
+      {
+        path: '/words/:id/edit',
+        name: 'edit',
+        component: Edit
+      },
+      {
+        path: '/test',
+        name: 'test',
+        component: Test
+      }
+];
 
 const router = new VueRouter({
   mode: 'history',
